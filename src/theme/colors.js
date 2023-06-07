8dea1c56932924eea88f3e5d10e9fedd111e8a95
bravobikes <c.ramirez@bravobikes.cc>
2023-06-07 14:16:06 -0600

    update color palette

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -16,18 +16,18 @@ export const neutral = {
   100: '#F3F4F6',
   200: '#E5E7EB',
   300: '#D2D6DB',
-  400: '#9DA4AE',
+  400: '#666666', // titulos sideabr
   500: '#6C737F',
   600: '#4D5761',
-  700: '#2F3746',
-  800: '#1C2536',
+  700: '#e88b00', // Sidebar darker color
+  800: '#ff9900', // sidebar
   900: '#111927'
 };
 
 export const indigo = withAlphas({
   lightest: '#F5F7FF',
   light: '#EBEEFE',
-  main: '#6366F1',
+  main: '#ff3300', // main ternium red graphs
   dark: '#4338CA',
   darkest: '#312E81',
   contrastText: '#FFFFFF'
