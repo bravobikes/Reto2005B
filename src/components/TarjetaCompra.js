b7488749abed17e70c234867b099f9316becc936
sebastian <sebramirezcordero@gmail.com>
2023-06-13 20:49:12 -0600

    Revert "caaaaaasi todo el styling de la tienda esta listo"
    This reverts commit d720ac1996a8c56e5c399cc006edc163c999de09.

--- src/components/TarjetaCompra.js
+++ src/components/TarjetaCompra.js
@@ -6,7 +6,6 @@ import {
     CardActions,
     CardContent,
     Divider,
-    Grid,
     Typography
   } from '@mui/material';
   import { useCallback, useState, useEffect } from 'react';
@@ -24,7 +23,7 @@ import {
   
   
   export const TarjetaCompra = () => {
-    // cambiar los requests para items de tienda en lugar de people si se implementa el API
+    
     const getEmployeeUrl = 'http://localhost:5000/getEmployee/';
     const deleteUrl = 'http://localhost:5000/deletepeople';
     const getSessionUserUrl = 'http://localhost:5000/getSessionUser';
@@ -69,11 +68,9 @@ import {
             <Avatar
               src={user.avatar}
               sx={{
-                height: 100,
+                height: 80,
                 mb: 2,
-                width: "100%",
-                borderRadius:"1em",
-                marginTop:0,
+                width: 80
               }}
             />
             <Typography
@@ -81,45 +78,25 @@ import {
               variant="h5"
             >
               {/* {user.name} */}
-              Nuevo Avatar
-              {/* {data.Name} */}
+              {data.Name}
             </Typography>
             <Typography
               color="text.secondary"
               variant="body2"
             >
               {/* {user.city} {user.country} */}
-              {/* {data.City} */}
+              {data.City}
             </Typography>
             <Typography
               color="text.secondary"
               variant="body2"
-              sx={{textAlign:"center"}}
             >
-              Desbloquea este avatar para tu perfil
-              {/* {user.timezone} */}
+              {user.timezone}
             </Typography>
           </Box>
         </CardContent>
         <Divider />
-        <CardActions sx={{marginTop:"3%"}}>
-          <Grid container spacing={1}>
-            <Grid item>
-            <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="gold" className="bi bi-coin" viewBox="0 0 16 16">
-              <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"/>
-              <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-              <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
-            </svg>
-            </Grid>
-            <Grid item>
-              <span>13</span>
-            </Grid>
-            
-          </Grid>
-          <Grid container spacing={1}>
-            <Button variant="contained" size="small">Comprar</Button>
-          </Grid>
-        
+        <CardActions>
           {/* aqui poner el precio y comprar maybe, ene sta fila al menos */}
         </CardActions>
       </Card>
--- src/sections/overview/overview-tienda.js
+++ src/sections/overview/overview-tienda.js
@@ -7,7 +7,6 @@ import {
     Card,
     CardActions,
     CardHeader,
-    Typography,
     Divider,
     IconButton,
     List,
@@ -21,40 +20,7 @@ export default function Tienda() {
     const players = [{username: "algo", puntosTotales: 123, img: "/assets/avatars/avatar-alcides-antonio.png"}];
     return (        
             <Card sx={{height:"100%"}}>
-                <CardHeader title={
-                    <Typography variant="h5" component="div">
-{/*                     
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1.5em" height="1.5em" fill="gold" className="bi bi-coin" viewBox="0 0 16 16">
-                        <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"/>
-                        <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                        <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
-                    </svg>
-                    Tienda */}
-                    <Grid container spacing={1} justifyContent="space-between" flexDirection="row">
-                        <Grid item>
-                            Tienda
-                        </Grid>
-                        <Grid item>
-                            <Grid container spacing={1} justifyContent="center" flexDirection="row">
-                                <Grid item>
-                                    {/* aqui cambiar la cantidad de monedas a las monedas que tiene el usuario */}
-                                    <div sx={{width:"1.5em", height:"1.5em"}}>13</div>
-                                    
-                                </Grid>
-                                <Grid item>
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="1.2em" height="1.2em" fill="gold" className="bi bi-coin" viewBox="0 0 16 16">
-                                    <path d="M5.5 9.511c.076.954.83 1.697 2.182 1.785V12h.6v-.709c1.4-.098 2.218-.846 2.218-1.932 0-.987-.626-1.496-1.745-1.76l-.473-.112V5.57c.6.068.982.396 1.074.85h1.052c-.076-.919-.864-1.638-2.126-1.716V4h-.6v.719c-1.195.117-2.01.836-2.01 1.853 0 .9.606 1.472 1.613 1.707l.397.098v2.034c-.615-.093-1.022-.43-1.114-.9H5.5zm2.177-2.166c-.59-.137-.91-.416-.91-.836 0-.47.345-.822.915-.925v1.76h-.005zm.692 1.193c.717.166 1.048.435 1.048.91 0 .542-.412.914-1.135.982V8.518l.087.02z"/>
-                                    <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z"/>
-                                    <path d="M8 13.5a5.5 5.5 0 1 1 0-11 5.5 5.5 0 0 1 0 11zm0 .5A6 6 0 1 0 8 2a6 6 0 0 0 0 12z"/>
-                                    </svg>
-                                </Grid>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                    
-                    
-                  </Typography>
-                }/>
+                <CardHeader title="Tienda"/>
                 <Grid container sx={{marginLeft:"1.5%"}}>
                     {/* creo que mejos, si no es los primeros 3 no agregar margin pero puede que se vea bien no se */}
                     <Grid item xs={12} sm={4}>
@@ -66,14 +32,13 @@ export default function Tienda() {
                     <Grid item xs={12} sm={4}>
                         <TarjetaCompra/>
                     </Grid>
-                    {/* agregar margin abajo de los ultimos 3 con javascript */}
-                    <Grid item xs={12} sm={4} sx={{marginTop:"4%", marginBottom:"4%"}}>
+                    <Grid item xs={12} sm={4} sx={{marginTop:"4%"}}>
                         <TarjetaCompra/>
                     </Grid>
-                    <Grid item xs={12} sm={4} sx={{marginTop:"4%", marginBottom:"4%"}}>
+                    <Grid item xs={12} sm={4} sx={{marginTop:"4%"}}>
                         <TarjetaCompra/>
                     </Grid>
-                    <Grid item xs={12} sm={4} sx={{marginTop:"4%", marginBottom:"4%"}}>
+                    <Grid item xs={12} sm={4} sx={{marginTop:"4%"}}>
                         <TarjetaCompra/>
                     </Grid>
                 </Grid>
@@ -84,4 +49,6 @@ export default function Tienda() {
    
     )
 }
+
+
 
