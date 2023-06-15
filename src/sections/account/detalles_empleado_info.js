f31066cad677a5d53c72e6d27a0dedc6aba8e901
bravobikes <c.ramirez@bravobikes.cc>
2023-06-15 16:57:56 -0600

    traducción a español

--- src/sections/account/detalles_empleado_info.js
+++ src/sections/account/detalles_empleado_info.js
@@ -62,7 +62,7 @@ export const AccountProfile = () => (
         fullWidth
         variant="text"
       >
-        Upload picture
+        Subir imagen
       </Button>
     </CardActions>
   </Card>
