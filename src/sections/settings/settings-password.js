d7c331f6c9eff30b738ec568e02a80ef45d14b50
bravobikes <c.ramirez@bravobikes.cc>
2023-06-15 16:54:37 -0600

    translate a español

--- src/sections/settings/settings-password.js
+++ src/sections/settings/settings-password.js
@@ -37,8 +37,8 @@ export const SettingsPassword = () => {
     <form onSubmit={handleSubmit}>
       <Card>
         <CardHeader
-          subheader="Update password"
-          title="Password"
+          subheader="Actualizar constraseña"
+          title="Contraseña"
         />
         <Divider />
         <CardContent>
@@ -48,7 +48,7 @@ export const SettingsPassword = () => {
           >
             <TextField
               fullWidth
-              label="Password"
+              label="Contraseña"
               name="password"
               onChange={handleChange}
               type="password"
@@ -56,7 +56,7 @@ export const SettingsPassword = () => {
             />
             <TextField
               fullWidth
-              label="Password (Confirm)"
+              label="Contraseña (Confirmar)"
               name="confirm"
               onChange={handleChange}
               type="password"
@@ -67,7 +67,7 @@ export const SettingsPassword = () => {
         <Divider />
         <CardActions sx={{ justifyContent: 'flex-end' }}>
           <Button variant="contained">
-            Update
+            Actualizar
           </Button>
         </CardActions>
       </Card>
