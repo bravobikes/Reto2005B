af0bfed35859fe0caa96a328ab9e94e2f0776523
bravobikes <c.ramirez@bravobikes.cc>
2023-06-15 19:24:02 -0600

    type

--- src/sections/overview/trainee/overview-areas-cursos-tomados.js
+++ src/sections/overview/trainee/overview-areas-cursos-tomados.js
@@ -86,7 +86,7 @@ export const OverviewTraffic = (props) => {
 
   return (
     <Card sx={sx}>
-      <CardHeader title="Araeas de cursos tomados" />
+      <CardHeader title="Areas de cursos tomados" />
       <CardContent>
         <Chart
           height={300}
